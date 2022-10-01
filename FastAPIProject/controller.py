e4df2a7944a60dbb803cce099ea41671ce48b21b
Mukesh Kumar <mr.mkumar1st@gmail.com>
2022-10-01 15:55:51 +0530

    FastAPI

---
+++ FastAPIProject/controller.py
@@ -0,0 +1,16 @@
+import json
+from fastapi import FastAPI
+
+import logging
+
+app = FastAPI()
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.get('/fast_api/test')
+def test():
+    logging.info("Logs ********************************************")
+    data = {"id": "123456",
+            "name": "Mukesh Kumar"
+            }
+    return json.dumps(data)
